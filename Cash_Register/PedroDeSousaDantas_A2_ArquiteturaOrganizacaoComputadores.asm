4aab83d2ac772289416a94e978ddc6c472646c57
Pedro Dantas <dev.pedrodantas@gmail.com>
2020-12-04 15:58:21 -0300

    Update PedroDeSousaDantas_A2_ArquiteturaOrganizacaoComputadores.asm

--- Cash_Register/PedroDeSousaDantas_A2_ArquiteturaOrganizacaoComputadores.asm
+++ Cash_Register/PedroDeSousaDantas_A2_ArquiteturaOrganizacaoComputadores.asm
@@ -1,15 +1,15 @@
 #####################################################################
 ## ATIVIDADE INDIVIDUAL AVALIATIVA(A2)
-## PROGRAMA«√O ASSEMBLY PARA ARQUITETURA MIPS32
+## PROGRAMA√á√ÉO ASSEMBLY PARA ARQUITETURA MIPS32
 ## Aluno: Pedro de Sousa Dantas
-## MatrÌcula:20162102395
+## Matr√≠cula:20162102395
 #####################################################################
 
 .data
 	msg1:  .asciiz "  Valor insuficiente para compra! "
 	msg2:  .asciiz "  Valor Suficiente! "
-	msg3:  .asciiz "  N„o h· troco! "
-	msg4:  .asciiz "  H· troco ou È insuficiente! "
+	msg3:  .asciiz "  N√£o h√° troco! "
+	msg4:  .asciiz "  H√° troco ou √© insuficiente! "
 	msg5:  .asciiz "  Valor pago em centavos: R$ "
 	msg6:  .asciiz "  Valor do produto em centavos: R$ "
 	msg7:  .asciiz "  Troco em centavos: R$ "
@@ -25,40 +25,40 @@
 	mo10:  .asciiz  " Quantidade de moedas de 0,10 centavos: "
 	mo5:   .asciiz   " Quantidade de moedas de 0,05 centavos: "
 	
-.text # DeclaraÁ„o de inÌcio do segmento de texto
-.globl main # DeclaraÁ„o de que o rÛtulo main È global
+.text # Declara√ß√£o de in√≠cio do segmento de texto
+.globl main # Declara√ß√£o de que o r√≥tulo main √© global
 
 main: 
 
-	# PersistÍncia dos valores nos registradores $sx da notas em centavos atravÈs da multiplicaÁ„o
+	# Persist√™ncia dos valores nos registradores $sx da notas em centavos 
 
 	li $s0, 1 # Quantidade de notas de R$20,00 inseridas.
-	mul $s0, $s0, 2000 # TransformaÁ„o do valor da nota em centavos
+	mul $s0, $s0, 2000 # Transforma√ß√£o do valor da nota em centavos
 	
 	li $s1, 1 #Quantidade de notas de R$10,00 inseridas.
-	mul $s1, $s1, 1000 # TransformaÁ„o do valor da nota em centavos
+	mul $s1, $s1, 1000 # Transforma√ß√£o do valor da nota em centavos
 	
 	li $s2, 1 #Quantidade de notas de R$5,00 inseridas.
-	mul $s2, $s2, 500 # TransformaÁ„o do valor da nota em centavos
+	mul $s2, $s2, 500 # Transforma√ß√£o do valor da nota em centavos
 	
 	li $s3, 1 #Quantidade de notas de R$2,00 inseridas.
-	mul $s3, $s3, 200 # TransformaÁ„o do valor da nota em centavos
+	mul $s3, $s3, 200 # Transforma√ß√£o do valor da nota em centavos
 	
 	li $s4, 1 #Quantidade de notas de R$1,00 inseridas.
-	mul $s4, $s4, 100 # TransformaÁ„o do valor da nota em centavos
+	mul $s4, $s4, 100 # Transforma√ß√£o do valor da nota em centavos
 	
 	li $s5, 1 #Quantidade de notas de R$0,50 inseridas.
-	mul $s5, $s5, 50 # TransformaÁ„o do valor da nota em centavos
+	mul $s5, $s5, 50 # Transforma√ß√£o do valor em centavos
 	
 	li $s6, 1 #Quantidade de notas de R$0,25 inseridas.
-	mul $s6, $s6, 25 # TransformaÁ„o do valor da nota em centavos
+	mul $s6, $s6, 25 # Transforma√ß√£o do valor em centavos
 	
 	li $s7, 1 #Quantidade de notas de R$0,10 inseridas.
-	mul $s7, $s7, 10 # TransformaÁ„o do valor da nota em centavos
+	mul $s7, $s7, 10 # Transforma√ß√£o do valor em centavos
 	
 	li, $k0, 5 # Moeda de 5 centavos
 	
-	# SomatÛrio dos valores inseridos de forma encadeada
+	# Somat√≥rio dos valores inseridos de forma encadeada
 	# formula geral => N = N0 + N1 + Nn 
 	# add $t2 = $s0 + $s1 + $n
 	add $t2, $s0, $s1 
@@ -73,15 +73,15 @@ main:
 	# Exemplo : R$20,15 = Valor do produto x 100
 	li $t9, 2015 
 	
-    ## VerificaÁ„o se h· capital para compra do produto ##
+    ## Verifica√ß√£o se h√° capital para compra do produto ##
 	###############################################################################################
 
 	# if (total > produto)
-	bgt $t2, $t9, true1 # Se total > produto for verdadeiro, a intruÁ„o pula para o label true1 
-	# Caso seja falso executa as instruÁıes abaixo
+	bgt $t2, $t9, true1 # Se total > produto for verdadeiro, a intru√ß√£o pula para o label true1 
+	# Caso seja falso executa as instru√ß√µes abaixo
 	li $v0, 4
 	la $a0, msg1
-	li $t0, 1 # retorno 1 quando o valor n„o È suficiente para compra
+	li $t0, 1 # retorno 1 quando o valor n√£o √© suficiente para compra
 	syscall 
 	b fim
 
@@ -93,35 +93,35 @@ main:
 	
 	###############################################################################################	
 	
-	# EspaÁo em branco entre as respostas
+	# Espa√ßo em branco entre as respostas
 	li $v0, 4
 	la $a0, enter 
 	syscall
     
-    ###############################################################################################
+        ###############################################################################################
     
 	# if (total < produto)
-	blt $t2, $t9, true2 # Se total < produto for verdadeiro, a instruÁ„o pula para o label true3
-	# Caso seja falso executa as instruÁıes abaixo
+	blt $t2, $t9, true2 # Se total < produto for verdadeiro, a instru√ß√£o pula para o label true3
+	# Caso seja falso executa as instru√ß√µes abaixo
 	li $v0, 4
 	la $a0, msg2
-	li $t0, 0 # retorno 1 quando o valor È suficiente para compra
+	li $t0, 0 # retorno 1 quando o valor √© suficiente para compra
 	syscall 
 	b fim
 	
     true2: 	# total < produto sendo true
 	li $v0, 4
 	la $a0, msg1
-    li $t0, 1 # retorno 1 quando o valor n„o È suficiente para compra
+    li $t0, 1 # retorno 1 quando o valor n√£o √© suficiente para compra
 	syscall
 		
 	###############################################################################################
 			
 	# if (produto = total)
-	beq $t2, $t9, true3 # Se total = produto for verdadeiro, a instruÁ„o pula para o label true2
-	# Caso seja falso executa as instruÁıes abaixo 
+	beq $t2, $t9, true3 # Se total = produto for verdadeiro, a instru√ß√£o pula para o label true2
+	# Caso seja falso executa as instru√ß√µes abaixo 
 	li $v0, 4
-	la $a0, msg4 # Imprime a mensagem "H· troco ou È insuficiente!"
+	la $a0, msg4 # Imprime a mensagem "H√° troco ou √© insuficiente!"
 	syscall 
 	b fim
 
@@ -134,49 +134,49 @@ main:
 	###############################################################################################
 	
 	fim:
-	# EspaÁo em branco entre as respostas
+	# Espa√ßo em branco entre as respostas
 	li $v0, 4
 	la $a0, enter 
 	syscall
 	
 	###############################################################################################
-    ## Impress„o do valor pago (em centavos) ##
+    ## Impress√£o do valor pago (em centavos) ##
 
-	li $v0, 4 # Carrega o comando 4 para impress„o
-	la $a0, msg5 # Impress„o da mensagem
+	li $v0, 4 # Carrega o comando 4 para impress√£o
+	la $a0, msg5 # Impress√£o da mensagem
 	syscall
-	li $v0, 1 # Carrega o comando 1 para impress„o de inteiro
-	move $a0, $t2 # Impress„o do valor pago
+	li $v0, 1 # Carrega o comando 1 para impress√£o de inteiro
+	move $a0, $t2 # Impress√£o do valor pago
 	syscall		
 	
-    ## Impress„o do valor do produto (em centavos) ##
-	# EspaÁo em branco entre as respostas
+    ## Impress√£o do valor do produto (em centavos) ##
+	# Espa√ßo em branco entre as respostas
 	li $v0, 4
 	la $a0, enter 
 	syscall
 	
-	li $v0, 4	# Carrega o comando 4 para impress„o
-	la $a0, msg6 # Impress„o de mensagem
+	li $v0, 4	# Carrega o comando 4 para impress√£o
+	la $a0, msg6 # Impress√£o de mensagem
 	syscall
-	li $v0, 1 # Carrega o comando 1 para impress„o de inteiro
-	move $a0, $t9 # Impress„o do valor do produto (em centavos)
+	li $v0, 1 # Carrega o comando 1 para impress√£o de inteiro
+	move $a0, $t9 # Impress√£o do valor do produto (em centavos)
 	syscall		
 	
-    ## Impress„o do troco (em centavos) ##
+    ## Impress√£o do troco (em centavos) ##
 	# troco = total - produto
 	sub $t3, $t2, $t9 
 	# $t3 = troco
 	
-	# EspaÁo em branco entre as respostas
+	# Espa√ßo em branco entre as respostas
 	li $v0, 4
 	la $a0, enter 
 	syscall
 	
-	li $v0, 4	# Carrega o comando 4 para impress„o
-	la $a0, msg7 # Impress„o de mensagem
+	li $v0, 4	# Carrega o comando 4 para impress√£o
+	la $a0, msg7 # Impress√£o de mensagem
 	syscall
-	li $v0, 1 # Carrega o comando 1 para impress„o de inteiro
-	move $a0, $t3 # Impress„o do valor do troco (em centavos)
+	li $v0, 1 # Carrega o comando 1 para impress√£o de inteiro
+	move $a0, $t3 # Impress√£o do valor do troco (em centavos)
 	syscall		
 	
     ## Algoritmo de calculo de troco a serem impressas ##
@@ -265,7 +265,7 @@ main:
 	
 	exit:
 	
-	# EspaÁo em branco entre as respostas
+	# Espa√ßo em branco entre as respostas
 	li $v0, 4
 	la $a0, enter 
 	syscall
@@ -274,115 +274,115 @@ main:
 	la $a0, enter 
 	syscall
 	
-	# Impress„o da quantidade de cedulas de 20 reais #
-	li $v0, 4	# Carrega o comando 4 para impress„o
-	la $a0, ced20 # Impress„o da mensagem
+	# Impress√£o da quantidade de cedulas de 20 reais #
+	li $v0, 4	# Carrega o comando 4 para impress√£o
+	la $a0, ced20 # Impress√£o da mensagem
 	syscall
-	li $v0, 1 # Carrega o comando 1 para impress„o de inteiro
-	move $a0, $a1 # Impress„o do valor
+	li $v0, 1 # Carrega o comando 1 para impress√£o de inteiro
+	move $a0, $a1 # Impress√£o do valor
 	syscall		
 	
-	# EspaÁo em branco entre as respostas
+	# Espa√ßo em branco entre as respostas
 	li $v0, 4
 	la $a0, enter
 	syscall
 	
-	## Impress„o da quantidade de cedulas de 10 reais ##
-	li $v0, 4	# Carrega o comando 4 para impress„o
-	la $a0, ced10 	# Impress„o da mensagem 	
+	## Impress√£o da quantidade de cedulas de 10 reais ##
+	li $v0, 4	# Carrega o comando 4 para impress√£o
+	la $a0, ced10 	# Impress√£o da mensagem 	
 	syscall
-	li $v0, 1 # Carrega o comando 1 para impress„o de inteiro
-	move $a0, $a2 # Impress„o do valor
+	li $v0, 1 # Carrega o comando 1 para impress√£o de inteiro
+	move $a0, $a2 # Impress√£o do valor
 	syscall		
 	
-	# EspaÁo em branco entre as respostas
+	# Espa√ßo em branco entre as respostas
 	li $v0, 4
 	la $a0, enter 
 	syscall
 	
-	## Impress„o da quantidade de cedulas de 5 reais ##
-	li $v0, 4	# Carrega o comando 4 para impress„o
-	la $a0, ced5  # Impress„o da mensagem  	
+	## Impress√£o da quantidade de cedulas de 5 reais ##
+	li $v0, 4	# Carrega o comando 4 para impress√£o
+	la $a0, ced5  # Impress√£o da mensagem  	
 	syscall
-	li $v0, 1 # Carrega o comando 1 para impress„o de inteiro
-	move $a0, $a3 # Impress„o do valor
+	li $v0, 1 # Carrega o comando 1 para impress√£o de inteiro
+	move $a0, $a3 # Impress√£o do valor
 	syscall		
 	
-	# EspaÁo em branco entre as respostas
+	# Espa√ßo em branco entre as respostas
 	li $v0, 4
 	la $a0, enter 
 	syscall
 	
-	## Impress„o da quantidade de cedulas de 2 reais ##
-	li $v0, 4	# Carrega o comando 4 para impress„o
-	la $a0, ced2 	# Impress„o da mensagem  	
+	## Impress√£o da quantidade de cedulas de 2 reais ##
+	li $v0, 4	# Carrega o comando 4 para impress√£o
+	la $a0, ced2 	# Impress√£o da mensagem  	
 	syscall
-	li $v0, 1 # Carrega o comando 1 para impress„o de inteiro
-	move $a0, $t4 # Impress„o do valor
+	li $v0, 1 # Carrega o comando 1 para impress√£o de inteiro
+	move $a0, $t4 # Impress√£o do valor
 	syscall	
 		
-	# EspaÁo em branco entre as respostas
+	# Espa√ßo em branco entre as respostas
 	li $v0, 4
 	la $a0, enter 
 	syscall
 	
-	## Impress„o da quantidade de cedulas de 1 reais ##
-	li $v0, 4	# Carrega o comando 4 para impress„o
-	la $a0, mo1 # Impress„o da mensagem  
+	## Impress√£o da quantidade de cedulas de 1 reais ##
+	li $v0, 4	# Carrega o comando 4 para impress√£o
+	la $a0, mo1 # Impress√£o da mensagem  
 	syscall
-	li $v0, 1 # Carrega o comando 1 para impress„o de inteiro
-	move $a0, $t5 # Impress„o do valor
+	li $v0, 1 # Carrega o comando 1 para impress√£o de inteiro
+	move $a0, $t5 # Impress√£o do valor
 	syscall	
 
-	# EspaÁo em branco entre as respostas
+	# Espa√ßo em branco entre as respostas
 	li $v0, 4
 	la $a0, enter 
 	syscall
 	
-	## Impress„o da quantidade de moedas de 50 centavos ##
-	li $v0, 4	# Carrega o comando 4 para impress„o
-	la $a0, mo50	# Impress„o da mensagem  	
+	## Impress√£o da quantidade de moedas de 50 centavos ##
+	li $v0, 4	# Carrega o comando 4 para impress√£o
+	la $a0, mo50	# Impress√£o da mensagem  	
 	syscall
-	li $v0, 1  # Carrega o comando 1 para impress„o de inteiro
-	move $a0, $t6 # Impress„o do valor
+	li $v0, 1  # Carrega o comando 1 para impress√£o de inteiro
+	move $a0, $t6 # Impress√£o do valor
 	syscall		
 	
-	# EspaÁo em branco entre as respostas
+	# Espa√ßo em branco entre as respostas
 	li $v0, 4
 	la $a0, enter 
 	syscall
 	
-	## Impress„o da quantidade de moedas de 25 centavos ##
-	li $v0, 4	# Carrega o comando 4 para impress„o
-	la $a0, mo25 	# Impress„o da mensagem	
+	## Impress√£o da quantidade de moedas de 25 centavos ##
+	li $v0, 4	# Carrega o comando 4 para impress√£o
+	la $a0, mo25 	# Impress√£o da mensagem	
 	syscall
-	li $v0, 1 # Carrega o comando 1 para impress„o de inteiro
-	move $a0, $t7 # Impress„o do valor
+	li $v0, 1 # Carrega o comando 1 para impress√£o de inteiro
+	move $a0, $t7 # Impress√£o do valor
 	syscall	
 	
-	# EspaÁo em branco entre as respostas
+	# Espa√ßo em branco entre as respostas
 	li $v0, 4
 	la $a0, enter 
 	syscall
 	
-	li $v0, 4	# Carrega o comando 4 para impress„o
-	la $a0, mo10 	# Impress„o da mensagem		
+	li $v0, 4	# Carrega o comando 4 para impress√£o
+	la $a0, mo10 	# Impress√£o da mensagem		
 	syscall
-	li $v0, 1 # Carrega o comando 1 para impress„o de inteiro
-	move $a0, $t8 # Impress„o do valor
+	li $v0, 1 # Carrega o comando 1 para impress√£o de inteiro
+	move $a0, $t8 # Impress√£o do valor
 	syscall	
 	
-	# EspaÁo em branco entre as respostas
+	# Espa√ßo em branco entre as respostas
 	li $v0, 4
 	la $a0, enter 
 	syscall
 	
-	## Impress„o da quantidade de moedas de 5 centavos ##
-	li $v0, 4	# Carrega o comando 4 para impress„o
-	la $a0, mo5  # Impress„o da mensagem		
+	## Impress√£o da quantidade de moedas de 5 centavos ##
+	li $v0, 4	# Carrega o comando 4 para impress√£o
+	la $a0, mo5  # Impress√£o da mensagem		
 	syscall 	
-	li $v0, 1 # Carrega o comando 1 para impress„o de inteiro
-	move $a0, $k1 # Impress„o do valor
+	li $v0, 1 # Carrega o comando 1 para impress√£o de inteiro
+	move $a0, $k1 # Impress√£o do valor
 	syscall	
 	
 	# return 0
